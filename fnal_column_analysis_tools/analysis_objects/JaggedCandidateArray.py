ba2838004b07396e40a2e39780f2105e0aa5b008
Lindsey Gray <Lindsey.Gray@cern.ch>
2018-12-02 01:38:36 -0600

    add workarounds for evaluating pt/eta/phi/mass quickly, will remove if it can be mitigated in awkward.JaggedArray

--- fnal_column_analysis_tools/analysis_objects/JaggedCandidateArray.py
+++ fnal_column_analysis_tools/analysis_objects/JaggedCandidateArray.py
@@ -1,6 +1,22 @@
 import awkward
 import uproot_methods
 
+import numpy as np
+
+#functions to quickly cash useful quantities
+def fast_pt(px,py):
+    return np.hypot(px,py)
+
+def fast_eta(px,py,pz):
+    p3mag = np.sqrt(px*px + py*py + pz*pz)
+    return np.arctanh(pz/p3mag)
+
+def fast_mass(px,py,pz,E):
+    return np.sqrt(E*E - (px*px + py*py + pz*pz))
+
+def fast_phi(px,py):
+    return np.arctan2(py,px)
+
 JaggedTLorentzVectorArray = awkward.Methods.mixin(uproot_methods.classes.TLorentzVector.ArrayMethods, awkward.JaggedArray)
 
 class JaggedCandidateMethods(awkward.Methods):
@@ -12,7 +28,12 @@ class JaggedCandidateMethods(awkward.Methods):
             items['p4'] = p4
         else:
             items['p4'] = uproot_methods.TLorentzVectorArray(p4[:,0],p4[:,1],
-                                                             p4[:,2],p4[:,3])        
+                                                             p4[:,2],p4[:,3])
+        thep4 = items['p4']
+        items['__fast_mass'] = fast_mass(thep4.x,thep4.y,thep4.z,thep4.t)
+        items['__fast_pt'] = fast_pt(thep4.x,thep4.y)
+        items['__fast_eta'] = fast_eta(thep4.x,thep4.y,thep4.z)
+        items['__fast_phi'] = fast_phi(thep4.x,thep4.y)
         items.update(kwargs)
         return cls.fromcounts(counts,awkward.Table(items))
     
@@ -20,6 +41,22 @@ class JaggedCandidateMethods(awkward.Methods):
     def p4(self):
         return self['p4']
     
+    @property
+    def mass(self):
+        return self['__fast_mass']
+
+    @property
+    def pt(self):
+        return self['__fast_pt']
+
+    @property
+    def eta(self):
+        return self['__fast_eta']
+
+    @property
+    def phi(self):
+        return self['__fast_phi']
+
     def at(self,what):
         thewhat = super(JaggedCandidateMethods,self).at(what)
         if 'p4' in thewhat.columns:
