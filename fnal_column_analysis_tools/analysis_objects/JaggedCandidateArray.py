2ca0f21955d69f400dbe779a3aeb0af37a61c8a6
Lindsey Gray <Lindsey.Gray@cern.ch>
2018-12-03 19:45:26 +0100

    small changes

--- fnal_column_analysis_tools/analysis_objects/JaggedCandidateArray.py
+++ fnal_column_analysis_tools/analysis_objects/JaggedCandidateArray.py
@@ -1,28 +1,40 @@
 import awkward
 import uproot_methods
-
 import numpy as np
 
-#functions to quickly cash useful quantities
-def fast_pt(px,py):
-    return np.hypot(px,py)
+JaggedTLorentzVectorArray = awkward.Methods.mixin(uproot_methods.classes.TLorentzVector.ArrayMethods, awkward.JaggedArray)
 
-def fast_eta(px,py,pz):
+#functions to quickly cash useful quantities
+def fast_pt(p4):
+    return np.hypot(p4.x,p4.y)
+
+def fast_eta(p4):
+    px = p4.x
+    py = p4.y
+    pz = p4.z
     p3mag = np.sqrt(px*px + py*py + pz*pz)
     return np.arctanh(pz/p3mag)
 
-def fast_mass(px,py,pz,E):
-    return np.sqrt(E*E - (px*px + py*py + pz*pz))
+def fast_phi(p4):
+    return np.arctan2(p4.y,p4.x)
 
-def fast_phi(px,py):
-    return np.arctan2(py,px)
-
-JaggedTLorentzVectorArray = awkward.Methods.mixin(uproot_methods.classes.TLorentzVector.ArrayMethods, awkward.JaggedArray)
+def fast_mass(p4):
+    px = p4.x
+    py = p4.y
+    pz = p4.z
+    en = p4.t
+    p3mag2 = (px*px + py*py + pz*pz)
+    return np.sqrt(np.abs(en*en - p3mag2))
 
 class JaggedCandidateMethods(awkward.Methods):
     
     @classmethod
     def candidatesfromcounts(cls,counts,p4,**kwargs):
+        offsets = awkward.array.jagged.counts2offsets(counts)
+        return cls.candidatesfromoffsets(offsets,p4,**kwargs)
+    
+    @classmethod
+    def candidatesfromoffsets(cls,offsets,p4,**kwargs):
         items = {}
         if isinstance(p4,uproot_methods.TLorentzVectorArray):
             items['p4'] = p4
@@ -30,50 +42,47 @@ class JaggedCandidateMethods(awkward.Methods):
             items['p4'] = uproot_methods.TLorentzVectorArray(p4[:,0],p4[:,1],
                                                              p4[:,2],p4[:,3])
         thep4 = items['p4']
-        items['__fast_mass'] = fast_mass(thep4.x,thep4.y,thep4.z,thep4.t)
-        items['__fast_pt'] = fast_pt(thep4.x,thep4.y)
-        items['__fast_eta'] = fast_eta(thep4.x,thep4.y,thep4.z)
-        items['__fast_phi'] = fast_phi(thep4.x,thep4.y)
+        items['__fast_pt'] = fast_pt(thep4)
+        items['__fast_eta'] = fast_eta(thep4)
+        items['__fast_phi'] = fast_phi(thep4)
+        items['__fast_mass'] = fast_mass(thep4)
         items.update(kwargs)
-        return cls.fromcounts(counts,awkward.Table(items))
+        return cls.fromoffsets(offsets,awkward.Table(items))
     
     @property
     def p4(self):
         return self['p4']
     
     @property
-    def mass(self):
-        return self['__fast_mass']
-
-    @property
     def pt(self):
         return self['__fast_pt']
-
+    
     @property
     def eta(self):
         return self['__fast_eta']
-
+    
     @property
     def phi(self):
         return self['__fast_phi']
-
+    
+    @property
+    def mass(self):
+        return self['__fast_mass']
+    
     def at(self,what):
         thewhat = super(JaggedCandidateMethods,self).at(what)
         if 'p4' in thewhat.columns:
             return self.fromjagged(thewhat)
         return thewhat
     
-    def distincts(self):
-        return self.pairs(same=False)
-    
     def pairs(self, same=True):
         outs = super(JaggedCandidateMethods, self).pairs(same)
-        thep4 = outs.at(0)['p4'] + outs.at(1)['p4']
-        outs['p4'] = thep4
-        outs['__fast_mass'] = awkward.JaggedArray.fromcounts(thep4.counts,fast_mass(thep4.x,thep4.y,thep4.z,thep4.t))
-        outs['__fast_pt'] = awkward.JaggedArray.fromcounts(thep4.counts,fast_pt(thep4.x,thep4.y))
-        outs['__fast_eta'] = awkward.JaggedArray.fromcounts(thep4.counts,fast_eta(thep4.x,thep4.y,thep4.z))
-        outs['__fast_phi'] = awkward.JaggedArray.fromcounts(thep4.counts,fast_phi(thep4.x,thep4.y))
+        outs['p4'] = outs.at(0)['p4'] + outs.at(1)['p4']
+        thep4 = outs['p4']
+        outs['__fast_pt'] = awkward.JaggedArray.fromoffsets(outs.offsets,fast_pt(thep4.content))
+        outs['__fast_eta'] = awkward.JaggedArray.fromoffsets(outs.offsets,fast_eta(thep4.content))
+        outs['__fast_phi'] = awkward.JaggedArray.fromoffsets(outs.offsets,fast_phi(thep4.content))
+        outs['__fast_mass'] = awkward.JaggedArray.fromoffsets(outs.offsets,fast_mass(thep4.content))
         return self.fromjagged(outs)
     
     def cross(self, other):
@@ -99,24 +108,16 @@ class JaggedCandidateMethods(awkward.Methods):
         keys = outs.columns
         for key in keys:
             if 'p4' not in outs.columns:
-                outs['p4'] = outs.at(int(key))['p4']
+                outs['p4'] = outs[key]['p4']
             else:
-                outs['p4'] = outs['p4'] + outs.at(int(key))['p4']
+                outs['p4'] = outs['p4'] + outs[key]['p4']
         thep4 = outs['p4']
-        p4content = thep4.content
-        outs['__fast_mass'] = awkward.JaggedArray.fromcounts(thep4.counts,fast_mass(p4content.x,
-                                                                                    p4content.y,
-                                                                                    p4content.z,
-                                                                                    p4content.t))
-        outs['__fast_pt'] = awkward.JaggedArray.fromcounts(thep4.counts,fast_pt(p4content.x,
-                                                                                p4content.y))
-        outs['__fast_eta'] = awkward.JaggedArray.fromcounts(thep4.counts,fast_eta(p4content.x,
-                                                                                  p4content.y,
-                                                                                  p4content.z))
-        outs['__fast_phi'] = awkward.JaggedArray.fromcounts(thep4.counts,fast_phi(p4content.x,
-                                                                                  p4content.y))
+        outs['__fast_pt'] = awkward.JaggedArray.fromoffsets(outs.offsets,fast_pt(thep4.content))
+        outs['__fast_eta'] = awkward.JaggedArray.fromoffsets(outs.offsets,fast_eta(thep4.content))
+        outs['__fast_phi'] = awkward.JaggedArray.fromoffsets(outs.offsets,fast_phi(thep4.content))
+        outs['__fast_mass'] = awkward.JaggedArray.fromoffsets(outs.offsets,fast_mass(thep4.content))
         return self.fromjagged(outs)
-
+        
     def __getattr__(self,what):
         if what in self.columns:
             return self[what]
