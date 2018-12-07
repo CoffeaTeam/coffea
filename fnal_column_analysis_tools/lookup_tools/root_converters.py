469c63247f28d3e6add038ed971e00332de55bf3
Lindsey Gray <lindsey.gray@gmail.com>
2018-12-07 12:25:00 +0100

    Converter for JSON-described histograms (#10)
    * json histo converter works in py27, 3 later
    
    * py27 and py3, also handles multiple values in json-histo
    
    * split all the file converters into their own files
    
    * allow file "type" in addition to format, update tests
    
    * variable order in json from shallow to deepest now

---
+++ fnal_column_analysis_tools/lookup_tools/root_converters.py
@@ -0,0 +1,33 @@
+import numpy as np
+import uproot
+
+TH1D = "<class 'uproot.rootio.TH1D'>"
+TH2D = "<class 'uproot.rootio.TH2D'>"
+TH1F = "<class 'uproot.rootio.TH1F'>"
+TH2F = "<class 'uproot.rootio.TH2F'>"
+RootDir = "<class 'uproot.rootio.ROOTDirectory'>"
+
+def convert_histo_root_file(file):
+    converted_file = {}
+    dumFile = uproot.open(file.strip())
+    for i, key in enumerate(dumFile.keys()):
+        histType = str(type(dumFile[key[:-2]]))
+        if histType == TH1D or histType == TH2D or histType==TH1F or histType==TH2F:
+            if  not ("bound method" in str(dumFile[key[:-2]].numpy)):
+                converted_file[key[:-2]] = dumFile[key[:-2]].numpy
+            else:
+                converted_file[key[:-2]] = dumFile[key[:-2]].numpy()
+        elif histType == RootDir: #means there are subdirectories wihin main directory
+            for j, key2 in enumerate(dumFile[key[:-2]].keys()):
+                histType2 = str(type(dumFile[key[:-2]][key2[:-2]]))
+                if histType2 == TH1D or histType2 == TH2D or histType2==TH1F or histTyp2==TH2F:
+                    if not ("bound method" in str(dumFile[key[:-2]][key2[:-2]].numpy)):
+                        converted_file[key[:-2]+'/'+key2[:-2]] = dumFile[key[:-2]][key2[:-2]].numpy
+                    else:
+                        converted_file[key[:-2]+'/'+key2[:-2]] = dumFile[key[:-2]][key2[:-2]].numpy()
+        else:
+            tempArrX= dumFile[key[:-2]]._fEX
+            tempArrY= dumFile[key[:-2]]._fEY
+            converted_file[key[:-2]] = [tempArrX, tempArrY]
+    return converted_file
+
