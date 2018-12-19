c7c0b96153457060de624939c87294d34d185e79
Nick Smith <nick.smith@cern.ch>
2018-12-19 13:08:34 -0600

    Hist tests finished

--- tests/test_hist_tools.py
+++ tests/test_hist_tools.py
@@ -31,7 +31,7 @@ def test_hist():
     assert h_cat_bins.project("vocalization", values=["woof", "ruff"]).values(errors=True)[("dog",)] == (101., np.sqrt(10001.))
 
     height = hist.Bin("height", "height [m]", 10, 0, 5)
-    h_mascots = hist.Hist("fermi mascot showdown",
+    h_mascots_1 = hist.Hist("fermi mascot showdown",
                           animal,
                           vocalization,
                           height,
@@ -40,19 +40,23 @@ def test_hist():
                         )
     adult_bison_h = np.random.normal(loc=2.5, scale=0.2, size=40)
     adult_bison_w = np.random.normal(loc=700, scale=100, size=40)
-    h_mascots.fill(animal="bison", vocalization="huff", height=adult_bison_h, mass=adult_bison_w)
+    h_mascots_1.fill(animal="bison", vocalization="huff", height=adult_bison_h, mass=adult_bison_w)
+    goose_h = np.random.normal(loc=0.4, scale=0.05, size=1000)
+    goose_w = np.random.normal(loc=7, scale=1, size=1000)
+    h_mascots_1.fill(animal="goose", vocalization="honk", height=goose_h, mass=goose_w)
+    crane_h = np.random.normal(loc=1, scale=0.05, size=4)
+    crane_w = np.random.normal(loc=10, scale=1, size=4)
+    h_mascots_1.fill(animal="crane", vocalization="none", height=crane_h, mass=crane_w)
+
+    h_mascots_2 = h_mascots_1.copy()
+    h_mascots_2.clear()
     baby_bison_h = np.random.normal(loc=.5, scale=0.1, size=20)
     baby_bison_w = np.random.normal(loc=200, scale=10, size=20)
     baby_bison_cutefactor = 2.5*np.ones_like(baby_bison_w)
-    h_mascots.fill(animal="bison", vocalization="baa", height=baby_bison_h, mass=baby_bison_w, weight=baby_bison_cutefactor)
-    goose_h = np.random.normal(loc=0.4, scale=0.05, size=1000)
-    goose_w = np.random.normal(loc=7, scale=1, size=1000)
-    h_mascots.fill(animal="goose", vocalization="honk", height=goose_h, mass=goose_w)
-    crane_h = np.random.normal(loc=1, scale=0.05, size=4)
-    crane_w = np.random.normal(loc=10, scale=1, size=4)
-    h_mascots.fill(animal="crane", vocalization="none", height=crane_h, mass=crane_w)
-    h_mascots.fill(animal="fox", vocalization="none", height=1., mass=30.)
+    h_mascots_2.fill(animal="bison", vocalization="baa", height=baby_bison_h, mass=baby_bison_w, weight=baby_bison_cutefactor)
+    h_mascots_2.fill(animal="fox", vocalization="none", height=1., mass=30.)
 
+    h_mascots = h_mascots_1 + h_mascots_2
     assert h_mascots.project("vocalization", values="h*").project("height").project("mass").project("animal").values()[()] == 1040.
 
     subphylums = {
@@ -66,10 +70,15 @@ def test_hist():
     nbirds_bin += np.sum((crane_h>=0.5)&(crane_h<1)&(crane_w>10)&(crane_w<100))
     assert h_subphylum.project("vocalization").values()[('birds',)][1,2] == nbirds_bin
     tally = h_subphylum.project("mass").project("height").project("vocalization").values()
-    assert tally[('birds',)] > tally[('mammals',)]
+    assert tally[('birds',)] == 1004.
+    assert tally[('mammals',)] == 91.
+
+    h_subphylum.scale({"honk": 0.1, "huff": 0.9}, axis="vocalization")
+    h_subphylum.scale(5.)
+    tally = h_subphylum.project("mass").project("height").project("vocalization").values()
+    assert tally[('birds',)] == 520.
+    assert tally[('mammals',)] == 435.
 
     assert h_subphylum.axis("vocalization") is vocalization
     assert h_subphylum.axis("height") is height
     assert h_subphylum.project("vocalization", values="h*").axis("height") is height
-
-    # TODO: test clear, add, scale methods
