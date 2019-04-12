e3b42705cefff13fd7416d66607a0787bf8a57af
Nick Smith <nick.smith@cern.ch>
2019-04-12 17:27:13 -0500

    Accumulator tests and updates

---
+++ tests/test_accumulators.py
@@ -0,0 +1,44 @@
+from __future__ import print_function, division
+from fnal_column_analysis_tools import processor
+
+
+def test_accumulators():
+    a = processor.accumulator(0.)
+    a += 3.
+    a += processor.accumulator(2)
+    assert a.value == 5.
+
+    b = processor.set_accumulator({'apples', 'oranges'})
+    b += {'pears'}
+    b += 'grapes'
+    assert b == {'apples', 'oranges', 'pears', 'grapes'}
+
+    c = processor.dict_accumulator({'num': a, 'fruit': b})
+    c['num'] += 2.
+    c += processor.dict_accumulator({
+        'num2': processor.accumulator(0),
+        'fruit': processor.set_accumulator({'apples', 'cherries'}),
+    })
+    assert c['num2'].value == 0
+    assert c['num'].value == 7.
+    assert c['fruit'] == {'apples', 'oranges', 'pears', 'grapes', 'cherries'}
+
+    d = processor.defaultdict_accumulator(lambda: processor.accumulator(0.))
+    d['x'] = processor.accumulator(0.) + 4
+    d['y'] += 5.
+    d['z'] += d['x']
+    d['x'] += d['y']
+    assert d['x'].value == 9.
+    assert d['y'].value == 5.
+    assert d['z'].value == 4.
+    assert d['w'].value == 0.
+
+    e = d + c
+
+    f = processor.defaultdict_accumulator(lambda: 2.)
+    f['x'] += 4.
+    assert f['x'] == 6.
+
+    f += f
+    assert f['x'] == 12.
+    assert f['y'] == 2.
