3368dcaab717747e17342575841bebc169c6ee7f
Lindsey Gray <lindsey.gray@gmail.com>
2019-05-20 10:04:23 -0500

    flake8 and such

--- fnal_column_analysis_tools/processor/parsl/condor_config.py
+++ fnal_column_analysis_tools/processor/parsl/condor_config.py
@@ -10,18 +10,19 @@ from parsl.executors import HighThroughputExecutor
 
 from parsl.addresses import address_by_hostname
 
+x509_proxy = 'x509up_u%s' % (os.getuid(), )
 
-def condor_config(cores_per_job=4,mem_per_core=2048,
+
+def condor_config(cores_per_job=4, mem_per_core=2048,
                   pyenv_dir='%s/.local' % (os.environ['HOME'], ),
                   grid_proxy_dir='/tmp/%s' % (x509_proxy, )):
-    x509_proxy = 'x509up_u%s'%(os.getuid())
     pyenv_relpath = pyenv_dir.split('/')[-1]
-    
+
     wrk_init = '''
     source /cvmfs/sft.cern.ch/lcg/views/LCG_95apython3/x86_64-centos7-gcc7-opt/setup.sh
     export PATH=`pwd`/%s:$PATH
     export PYTHONPATH=`pwd`/%s:$PYTHONPATH
-    
+
     export X509_USER_PROXY=`pwd`/%s
     mkdir -p ./htex_Local
     ''' % ('%s/bin' % pyenv_relpath,
@@ -32,7 +33,7 @@ def condor_config(cores_per_job=4,mem_per_core=2048,
     transfer_output_files = htex_Local
     RequestMemory = %d
     RequestCpus = %d
-    ''' % (mem_per_core*cores_per_job, cores_per_job)
+    ''' % (mem_per_core * cores_per_job, cores_per_job)
 
     xfer_files = [pyenv_dir, '/tmp/%s' % (x509_proxy, )]
 
@@ -43,20 +44,20 @@ def condor_config(cores_per_job=4,mem_per_core=2048,
                 address=address_by_hostname(),
                 prefetch_capacity=0,
                 cores_per_worker=1,
-                max_workers=nproc,
+                max_workers=cores_per_job,
                 worker_logdir_root='./',
                 provider=CondorProvider(
-                channel=LocalChannel(),
-                init_blocks=24,
-                max_blocks=200,
-                nodes_per_block=1,
-                worker_init = wrk_init,
-                transfer_input_files=xfer_files,
-                scheduler_options=condor_cfg
+                    channel=LocalChannel(),
+                    init_blocks=24,
+                    max_blocks=200,
+                    nodes_per_block=1,
+                    worker_init=wrk_init,
+                    transfer_input_files=xfer_files,
+                    scheduler_options=condor_cfg
                 ),
             )
         ],
         strategy=None,
-        )
-    
+    )
+
     return condor_htex
