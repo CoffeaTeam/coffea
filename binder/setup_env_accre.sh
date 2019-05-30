79243b6fe0c944d35315e38785e30869c3dce664
lgray <Lindsey.Gray@cern.ch>
2019-05-30 17:52:52 +0000

    works

--- binder/setup_env_accre.sh
+++ binder/setup_env_accre.sh
@@ -4,7 +4,7 @@ python3.6 -m virtualenv py36
 source py36/bin/activate
 
 pip install --upgrade pip
-pip install llvmlite numba numpy pandas ipykernel matplotlib lz4 pyarrow tqdm py4j
+pip install cloudpickle jinja2 llvmlite numba numpy pandas ipykernel matplotlib lz4 pyarrow tqdm py4j
 
 pip install uproot
 pip install --upgrade uproot-methods
@@ -12,24 +12,25 @@ pip install --upgrade uproot-methods
 pip install --upgrade numpy scipy
 
 # get dependencies for it
-pip install fnal-column-analysis-tools --upgrade
+pip install fnal-column-analysis-tools[parsl,spark] --upgrade
 # get latest and greatest
-pip install https://github.com/Parsl/parsl/zipball/master --user
-git clone -b topic_hats_scaleout git@github.com:CoffeaTeam/fnal-column-analysis-tools.git
+pip install https://github.com/Parsl/parsl/zipball/master
+git clone -b topic_hats_scaleout https://github.com/CoffeaTeam/fnal-column-analysis-tools.git
 
 # progressbar, sliders, etc.
 jupyter nbextension enable --py widgetsnbextension
 
 # issue with python3 bindings, see https://sft.its.cern.ch/jira/browse/SPI-1198
-wget https://github.com/xrootd/xrootd/archive/v4.8.4.tar.gz
-tar zxf v4.8.4.tar.gz && rm -f v4.8.4.tar.gz
-cp xrd_setup.py xrootd-4.8.4/bindings/python/
-pushd xrootd-4.8.4/bindings/python/
-python xrd_setup.py install --user
+wget https://raw.githubusercontent.com/lgray/coffeandbacon/master/xrd_setup.py
+wget https://github.com/xrootd/xrootd/archive/v4.8.5.tar.gz
+tar zxf v4.8.5.tar.gz && rm -f v4.8.5.tar.gz
+cp xrd_setup.py xrootd-4.8.5/bindings/python/
+pushd xrootd-4.8.5/bindings/python/
+python xrd_setup.py install
 popd
-rm -rf xrootd-4.8.4
+rm -rf xrootd-4.8.5
 
-ipython kernel install --user --name "py36" --display-name "py36"
+ipython kernel install --user --name "py36_test" --display-name "py36_test"
 
 deactivate
 
