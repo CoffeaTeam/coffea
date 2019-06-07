998b7d89542c1473af9805cf667e2d02902c09aa
Nick Smith <nick.smith@cern.ch>
2019-06-07 15:07:10 -0500

    Introduce .coffea files, coffea.util.load, coffea.util.save

--- coffea/util.py
+++ coffea/util.py
@@ -4,9 +4,28 @@ except ImportError:
     import awkward
 
 from awkward.util import numpy
-
 import numba
 
 akd = awkward
 np = numpy
 nb = numba
+
+import lz4.frame
+import cloudpickle
+
+def load(filename):
+    '''
+    Load a coffea file from disk
+    '''
+    with lz4.frame.open(filename) as fin:
+        output = cloudpickle.load(fin)
+    return output
+
+
+def save(output, filename):
+    '''
+    Save a coffea object or collection thereof to disk
+    Suggested suffix: .coffea
+    '''
+    with lz4.frame.open(filename, 'wb') as fout:
+        cloudpickle.dump(output, fout)
