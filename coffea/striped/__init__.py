e12f8a72b703d49752a37ce916ab7ab1c672e922
Lindsey Gray <lindsey.gray@gmail.com>
2019-09-29 14:59:07 -0500

    remove unused/stale striped package

--- coffea/striped/__init__.py
+++
@@ -1,5 +0,0 @@
-from .StripedColumnTransformer import ColumnGroup
-from .StripedColumnTransformer import PhysicalColumnGroup
-from .ColumnGroup2JaggedTable import jaggedFromColumnGroup
-from .Callback import HistCollectorCallback
-from .UprootAdaptor import UprootJob
