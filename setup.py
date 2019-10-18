beacc185526f1f617efe4b0035b188bf38efd5db
andrzejnovak <novak5andrzej@gmail.com>
2019-10-18 16:52:20 +0100

    bump v

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ INSTALL_REQUIRES = ['awkward>=0.8.4',
                     'lz4',
                     'six',
                     'cloudpickle',
-                    'mplhep>=0.0.10',
+                    'mplhep>=0.0.11',
                     ]
 EXTRAS_REQUIRE = {}
 if six.PY3:
