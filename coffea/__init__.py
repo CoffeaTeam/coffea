a1b0b2792c14c5ee1122a08dce2659e8d76b4954
Lindsey Gray <lindsey.gray@gmail.com>
2019-12-20 11:06:51 -0600

    preserve python2 support in a few converters

--- coffea/__init__.py
+++ coffea/__init__.py
@@ -32,7 +32,9 @@ __version__ = version.__version__
 
 import sys
 if sys.version_info.major < 3:
-    raise RuntimeError('coffea only supports python3 as of 1 January 2020')
+    import warnings
+    warnings.warn('coffea only supports python3 as of 1 January 2020!')
+    warnings.warn('If you are using python2 and run into problems please submit a pull requet to fix the issue!')
 
 from . import (
     hist,
