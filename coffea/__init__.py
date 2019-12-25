6446b74ffc39813f76bff8b337f3177b4ad72ea2
Lindsey Gray <lindsey.gray@gmail.com>
2019-12-25 15:00:42 -0600

    typo

--- coffea/__init__.py
+++ coffea/__init__.py
@@ -34,7 +34,7 @@ import sys
 if sys.version_info.major < 3:
     import warnings
     warnings.warn('coffea only supports python3 as of 1 January 2020!')
-    warnings.warn('If you are using python2 and run into problems please submit a pull requet to fix the issue!')
+    warnings.warn('If you are using python2 and run into problems please submit a pull request to fix the issue!')
 
 from . import (
     hist,
