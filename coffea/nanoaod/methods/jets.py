9052147d457ba7af65ba0825413d18e9fb58d709
Nick Smith <nick.smith@cern.ch>
2020-02-02 17:34:57 +0100

    Add jet ID as a property

--- coffea/nanoaod/methods/jets.py
+++ coffea/nanoaod/methods/jets.py
@@ -5,6 +5,12 @@ import awkward
 class FatJet(LorentzVector):
     '''NanoAOD large radius jet object'''
     subjetmap = {'FatJet': 'SubJet'}  # V6 has 'GenJetAK8': 'SubGenJetAK8', maybe better to put in generator.py
+    LOOSE = 0
+    'jetId bit position'
+    TIGHT = 1
+    'jetId bit position'
+    TIGHTLEPVETO = 2
+    'jetId bit position'
 
     def _finalize(self, name, events):
         subjets = self.subjetmap.get(name, None)
@@ -20,10 +26,32 @@ class FatJet(LorentzVector):
             del self['subJetIdx1']
             del self['subJetIdx2']
 
+    @property
+    def isLoose(self):
+        '''Returns a boolean array marking loose jets according to jetId index'''
+        return (self.jetId & (1 << self.LOOSE)).astype(bool)
+
+    @property
+    def isTight(self):
+        '''Returns a boolean array marking tight jets according to jetId index'''
+        return (self.jetId & (1 << self.TIGHT)).astype(bool)
+
+    @property
+    def isTightLeptonVeto(self):
+        '''Returns a boolean array marking tight jets with explicit lepton veto according to jetId index'''
+        return (self.cutBasedBitmap & (1 << self.TIGHTLEPVETO)).astype(bool)
+
 
 class Jet(LorentzVector):
     '''NanoAOD narrow radius jet object'''
-    _enable_genjet = False  # some issue with indexing in nano v5?
+    _enable_genjet = False
+    'Set to true if using NanoAODv6 or newer (v5 had a bug in the mapping)'
+    LOOSE = 0
+    'jetId bit position'
+    TIGHT = 1
+    'jetId bit position'
+    TIGHTLEPVETO = 2
+    'jetId bit position'
 
     def _finalize(self, name, events):
         if 'Electron' in events.columns:
@@ -68,3 +96,18 @@ class Jet(LorentzVector):
             del self['genJetIdx']
 
         self.type.check = False
+
+    @property
+    def isLoose(self):
+        '''Returns a boolean array marking loose jets according to jetId index'''
+        return (self.jetId & (1 << self.LOOSE)).astype(bool)
+
+    @property
+    def isTight(self):
+        '''Returns a boolean array marking tight jets according to jetId index'''
+        return (self.jetId & (1 << self.TIGHT)).astype(bool)
+
+    @property
+    def isTightLeptonVeto(self):
+        '''Returns a boolean array marking tight jets with explicit lepton veto according to jetId index'''
+        return (self.cutBasedBitmap & (1 << self.TIGHTLEPVETO)).astype(bool)
