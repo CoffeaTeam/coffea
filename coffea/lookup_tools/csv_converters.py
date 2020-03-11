edd9eebc9f7727e3ceef729d4cc228c4e0cc7286
Peter Fackeldey <peter.fackeldey@rwth-aachen.de>
2020-03-11 15:53:44 +0100

    fix cornercases where no formulas are found or raise an Exception when multiple fomulas are found for the same bin

--- coffea/lookup_tools/csv_converters.py
+++ coffea/lookup_tools/csv_converters.py
@@ -58,11 +58,26 @@ def convert_btag_csv_file(csvFilePath):
         for i, eta_bin in enumerate(etaBins[:-1]):
             for j, pt_bin in enumerate(ptBins[:-1]):
                 for k, discr_bin in enumerate(discrBins[:-1]):
-                    this_bin = np.where((all_names == label) &
-                                        (corrections[columns[4]] == eta_bin) &
-                                        (corrections[columns[6]] == pt_bin) &
-                                        (corrections[columns[8]] == discr_bin))
-                    vals[k, j, i] = corrections[this_bin][columns[10]][0]
+                    this_bin = np.where(
+                        (all_names == label)
+                        & (corrections[columns[4]] == eta_bin)
+                        & (corrections[columns[6]] == pt_bin)
+                        & (corrections[columns[8]] == discr_bin)
+                    )[0]
+                    if len(this_bin) == 1:
+                        vals[k, j, i] = corrections[this_bin][columns[10]][0]
+                    elif len(this_bin) > 1:
+                        raise Exception(
+                            'Multiple formulas for the same bin: label={label} eta_bin={eta_bin} pt_bin={pt_bin} discr_bin={discr_bin}'.format(
+                                label=label,
+                                eta_bin=eta_bin,
+                                pt_bin=pt_bin,
+                                discr_bin=discr_bin,
+                            )
+                        )
+                    else:
+                        warnings.warn('Bin without formula. Setting scale factor to 1', RuntimeWarning)
+                        vals[k, j, i] = '1.'
         label_decode = []
         for i in range(len(label)):
             label_decode.append(label[i])
