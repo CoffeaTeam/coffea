105876182e60adcee65e97ef0648a907f6858d65
Lindsey Gray <lindsey.gray@gmail.com>
2020-03-12 16:07:44 -0500

    remove print statments

--- tests/test_lookup_tools.py
+++ tests/test_lookup_tools.py
@@ -153,7 +153,8 @@ def test_jec_txt_scalefactors():
         "* * tests/samples/Spring16_25nsV10_MC_SF_AK4PFPuppi.jersf.txt",
         "* * tests/samples/Autumn18_V7b_MC_SF_AK8PFchs.jersf.txt.gz",
         "* * tests/samples/Fall17_17Nov2017_V32_MC_L2Relative_AK4Calo.jec.txt.gz",
-        "* * tests/samples/Fall17_17Nov2017_V32_MC_L1JPTOffset_AK4JPT.jec.txt.gz"
+        "* * tests/samples/Fall17_17Nov2017_V32_MC_L1JPTOffset_AK4JPT.jec.txt.gz",
+        "* * tests/samples/Fall17_17Nov2017B_V32_DATA_L2Relative_AK4Calo.txt"
     ])    
     extractor.finalize()
 
