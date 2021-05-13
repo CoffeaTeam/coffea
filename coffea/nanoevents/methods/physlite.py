c09fefa34cda6b97ad9464cdd816117cf8056617
Nikolai Hartmann <nikoladze@posteo.de>
2021-05-13 08:15:10 +0200

    use full package names

--- coffea/nanoevents/methods/physlite.py
+++ coffea/nanoevents/methods/physlite.py
@@ -1,5 +1,5 @@
-import awkward as ak
-import numpy as np
+import awkward
+import numpy
 from coffea.nanoevents.methods import vector
 
 
@@ -7,14 +7,14 @@ behavior = {}
 behavior.update(vector.behavior)
 
 
-@ak.mixin_class(behavior)
+@awkward.mixin_class(behavior)
 class xAODParticle(vector.PtEtaPhiMLorentzVector):
     @property
     def mass(self):
         return self.m
 
 
-@ak.mixin_class(behavior)
+@awkward.mixin_class(behavior)
 class xAODTrackParticle(vector.LorentzVector):
     "see https://gitlab.cern.ch/atlas/athena/-/blob/21.2/Event/xAOD/xAODTracking/Root/TrackParticle_v1.cxx#L82"
 
@@ -28,20 +28,20 @@ class xAODTrackParticle(vector.LorentzVector):
 
     @property
     def p(self):
-        return 1.0 / np.abs(self.qOverP)
+        return 1.0 / numpy.abs(self.qOverP)
 
     @property
     def x(self):
-        return self.p * np.sin(self.theta) * np.cos(self.phi)
+        return self.p * numpy.sin(self.theta) * numpy.cos(self.phi)
 
     @property
     def y(self):
-        return self.p * np.sin(self.theta) * np.sin(self.phi)
+        return self.p * numpy.sin(self.theta) * numpy.sin(self.phi)
 
     @property
     def z(self):
-        return self.p * np.cos(self.theta)
+        return self.p * numpy.cos(self.theta)
 
     @property
     def t(self):
-        return np.sqrt(139.570 ** 2 + self.x ** 2 + self.y ** 2 + self.z ** 2)
+        return numpy.sqrt(139.570 ** 2 + self.x ** 2 + self.y ** 2 + self.z ** 2)
