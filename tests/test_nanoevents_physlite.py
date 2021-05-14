fc26a14a659949976cd841168e8c37488a182dd9
Nikolai Hartmann <nikoladze@posteo.de>
2021-05-14 17:59:25 +0200

    add test for electron track links

---
+++ tests/test_nanoevents_physlite.py
@@ -0,0 +1,19 @@
+import awkward as ak
+import os
+from coffea.nanoevents import NanoEventsFactory, PHYSLITESchema
+
+
+def test_electron_track_links():
+    path = os.path.abspath(f"tests/samples/DAOD_PHYSLITE_21.2.108.0.art.pool.root")
+    factory = NanoEventsFactory.from_root(
+        path, treepath="CollectionTree", schemaclass=PHYSLITESchema
+    )
+    events = factory.events()
+    for event in events:
+        for electron in event.Electrons:
+            for link_index, link in enumerate(electron.trackParticleLinks):
+                track_index = link.m_persIndex
+                assert (
+                    event.GSFTrackParticles[track_index].z0
+                    == electron.trackParticles[link_index].z0
+                )
