2d843ddb962b8baf1f7ef00fc02252d657ea9161
Nikolai Hartmann <nikoladze@posteo.de>
2021-05-14 16:58:01 +0200

    add temporary hack for ElementLink data types

--- coffea/nanoevents/schemas/physlite.py
+++ coffea/nanoevents/schemas/physlite.py
@@ -45,6 +45,17 @@ class PHYSLITESchema(BaseSchema):
             top_key = key_fields[0]
             sub_key = ".".join(key_fields[1:])
             objname = top_key.replace("Analysis", "").replace("AuxDyn", "")
+
+            # temporary hack to have the correct type for the ElementLinks
+            # (uproot loses the type information somewhere on the way and they end up int64)
+            try:
+                for k in ["m_persIndex", "m_persKey"]:
+                    form = ak_form["content"]["content"]["contents"][k]
+                    form["itemsize"] = 8
+                    form["primitive"] = "int64"
+            except KeyError:
+                pass
+
             zip_groups[objname].append(((key, sub_key), ak_form))
 
             # Global indices
