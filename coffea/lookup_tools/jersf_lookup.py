4aa7376d114ef0cce772abc1359d67f6af5ba976
Benjamin Fischer <benjamin.fischer@cern.ch>
2021-08-04 17:37:04 +0200

    Manually remove dangling 0-dimension

--- coffea/lookup_tools/jersf_lookup.py
+++ coffea/lookup_tools/jersf_lookup.py
@@ -98,9 +98,11 @@ class jersf_lookup(lookup_base):
 
         # get parameter values
         parm_values = numpy.stack(
-            [numpy.array(parm[..., 0][bin_tuple]) for parm in self._parms],
+            [numpy.array(parm[bin_tuple]).squeeze() for parm in self._parms],
             axis=1,
         )
+        if parm_values.shape[2:] == (0,):
+            parm_values.shape = parm_values.shape[:2]
         return parm_values
 
     @property
