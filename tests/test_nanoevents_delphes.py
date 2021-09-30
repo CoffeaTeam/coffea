0705126d37da681e6bb4bd0ea4e6bb4f64c0c02e
Giordon Stark <kratsg@gmail.com>
2021-09-29 17:20:31 -0700

    flesh out tests more

--- tests/test_nanoevents_delphes.py
+++ tests/test_nanoevents_delphes.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 from coffea.nanoevents import NanoEventsFactory, DelphesSchema
+import awkward as ak
 
 
 @pytest.fixture(scope="module")
@@ -52,3 +53,96 @@ def events():
 )
 def test_collection_exists(events, collection):
     assert hasattr(events, collection)
+
+
+@pytest.mark.parametrize(
+    "collection",
+    [
+        "CaloJet02",
+        "CaloJet04",
+        "CaloJet08",
+        "CaloJet15",
+        "GenJet",
+        "GenJet02",
+        "GenJet04",
+        "GenJet08",
+        "GenJet15",
+        "Jet",
+        "ParticleFlowJet02",
+        "ParticleFlowJet04",
+        "ParticleFlowJet08",
+        "ParticleFlowJet15",
+        "TrackJet02",
+        "TrackJet04",
+        "TrackJet08",
+        "TrackJet15",
+    ],
+)
+def test_lorentz_vectorization(collection, events):
+    mask = ak.num(events[collection]) > 0
+    assert (
+        ak.type(events[collection][mask][0, 0].Area).parameters["__record__"]
+        == "LorentzVector"
+    )
+    assert (
+        ak.type(events[collection][mask][0, 0].SoftDroppedJet).parameters["__record__"]
+        == "LorentzVector"
+    )
+    assert (
+        ak.type(events[collection][mask][0, 0].SoftDroppedSubJet1).parameters[
+            "__record__"
+        ]
+        == "LorentzVector"
+    )
+    assert (
+        ak.type(events[collection][mask][0, 0].SoftDroppedSubJet2).parameters[
+            "__record__"
+        ]
+        == "LorentzVector"
+    )
+
+
+@pytest.mark.parametrize(
+    "collection",
+    [
+        "CaloJet02",
+        "CaloJet04",
+        "CaloJet08",
+        "CaloJet15",
+        "GenJet",
+        "GenJet02",
+        "GenJet04",
+        "GenJet08",
+        "GenJet15",
+        "Jet",
+        "ParticleFlowJet02",
+        "ParticleFlowJet04",
+        "ParticleFlowJet08",
+        "ParticleFlowJet15",
+        "TrackJet02",
+        "TrackJet04",
+        "TrackJet08",
+        "TrackJet15",
+    ],
+)
+def test_nested_lorentz_vectorization(collection, events):
+    mask = ak.num(events[collection]) > 0
+    assert ak.all(ak.num(events[collection].PrunedP4_5, axis=2) == 5)
+    assert (
+        ak.type(events[collection][mask].PrunedP4_5[0, 0, 0]).parameters["__record__"]
+        == "LorentzVector"
+    )
+
+    assert ak.all(ak.num(events[collection].SoftDroppedP4_5, axis=2) == 5)
+    assert (
+        ak.type(events[collection][mask].SoftDroppedP4_5[0, 0, 0]).parameters[
+            "__record__"
+        ]
+        == "LorentzVector"
+    )
+
+    assert ak.all(ak.num(events[collection].TrimmedP4_5, axis=2) == 5)
+    assert (
+        ak.type(events[collection][mask].TrimmedP4_5[0, 0, 0]).parameters["__record__"]
+        == "LorentzVector"
+    )
