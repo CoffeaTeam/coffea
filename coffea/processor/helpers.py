3a6e7e1e4cbdd85a85fa9859a563bc487bf5e189
Nick Smith <nick.smith@cern.ch>
2021-12-10 17:19:52 -0600

    Emit deprecations as proper exceptions
    So that coffea.deprecations_as_errors = True works as advertised

--- coffea/processor/helpers.py
+++ coffea/processor/helpers.py
@@ -20,7 +20,7 @@ class Weights(object):
 
     def __init__(self, size, storeIndividual=False):
         deprecate(
-            "This utility has moved to the `coffea.analysis_tools` subpackage and has new features, check it out!",
+            RuntimeError("This utility has moved to the `coffea.analysis_tools` subpackage and has new features, check it out!"),
             0.8,
         )
         self._weight = numpy.ones(size)
@@ -173,7 +173,7 @@ class PackedSelection(object):
         TODO: extend to multi-column for arbitrary bit depth
         """
         deprecate(
-            "This utility has moved to the `coffea.analysis_tools` subpackage and has new features, check it out!",
+            RuntimeError("This utility has moved to the `coffea.analysis_tools` subpackage and has new features, check it out!"),
             0.8,
         )
         self._dtype = numpy.dtype(dtype)
