71a4527ee8589c4a5181cf1a92ab7928ad9b70d0
Nicholas Smith <nick.smith@cern.ch>
2022-06-22 10:13:50 -0500

    Avoid uproot release with API change
    See #686

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ def get_description():
 
 INSTALL_REQUIRES = [
     "awkward>=1.5.1,<2",
-    "uproot>=4.1.6",
+    "uproot>=4.1.6,==4.*,!=4.2.4",
     "uproot3-methods>=0.10.0",
     "uproot3>=3.14.1",
     "correctionlib>=2.0.0",
