cea6bdad55eb7c2fea064440cd570a288acf2147
Lindsey Gray <lindsey.gray@gmail.com>
2022-10-27 16:01:04 -0500

    put in a formal deprecation for old csv converter

--- coffea/lookup_tools/csv_converters.py
+++ coffea/lookup_tools/csv_converters.py
@@ -7,10 +7,7 @@ btag_feval_dims = {0: [1], 1: [1], 2: [1], 3: [2]}
 
 
 def convert_btag_csv_file(csvFilePath):
-    warnings.warn(
-        "Auto-conversion of btag CSV files is deprecated. Try coffea.btag_tools.BTagScaleFactor!",
-        FutureWarning,
-    )
+    deprecate(ImportError("Auto-conversion of btag CSV files is deprecated and will be removed. Try coffea.btag_tools.BTagScaleFactor or correctionlib!"), "v0.8.0", "31 Dec 2022")
 
     fopen = open
     fmode = "rt"
