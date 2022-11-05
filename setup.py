d55c3b0b4b556910b407ca3a9cfdc68315d4ef31
Matthew Feickert <matthew.feickert@cern.ch>
2022-11-05 18:59:37 -0500

    docs: Add Python 3.10 classifier to PyPI metadata

--- setup.py
+++ setup.py
@@ -144,6 +144,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Information Analysis",
         "Topic :: Scientific/Engineering :: Mathematics",
