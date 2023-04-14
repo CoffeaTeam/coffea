43e5cd84454e7a0371f6ada09d698311a5747123
Lindsey Gray <lindsey.gray@gmail.com>
2023-04-14 11:26:05 -0500

    use uproot chunking for uproot.dask, repin uproot

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ def get_description():
 
 INSTALL_REQUIRES = [
     "awkward>=2.1.3",
-    "uproot>=5.0.5",
+    "uproot>=5.0.7",
     "dask[array]>=2022.12.1",
     "dask-awkward>=2023.4.1",
     "dask-histogram>=2023.4.1",
