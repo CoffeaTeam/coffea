9078d09313ede16a005d91c2c59046df747156e5
Yi-Mu Chen <enochnotsocool@gmail.com>
2023-04-26 11:57:32 +0200

    Fixed merge conflicts

--- coffea/ml_tools/torch_wrapper.py
+++ coffea/ml_tools/torch_wrapper.py
@@ -11,7 +11,7 @@ try:
 except ImportError as err:
     warnings.warn(
         "Users should make sure the torch package is installed before proceeding!",
-        UserWarning
+        UserWarning,
     )
     raise err
 
@@ -130,11 +130,6 @@ class torch_wrapper(abc.ABC):
                 self.twrap = twrap
 
             def __call__(self, *args):
-<<<<<<< HEAD
-                # Call function can only receive args
-=======
-                # Call function can only recieve args
->>>>>>> fd00f58
                 if awkward.backend(args[0]) == "typetracer":
                     # For meta-data extraction
                     eval_args, eval_kwargs = self.args_to_pair(*args)
