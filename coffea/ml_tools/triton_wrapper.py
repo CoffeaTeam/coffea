03a75ff2bf657ff3ec38da16f7f7dce1cd4286b7
Yi-Mu Chen <enochnotsocool@gmail.com>
2023-04-26 13:54:26 +0200

    Have triton_wrapper us the new base class

--- coffea/ml_tools/triton_wrapper.py
+++ coffea/ml_tools/triton_wrapper.py
@@ -21,10 +21,10 @@ except ImportError as err:
     )
     raise err
 
-from .helper import convert_args_pair, lazy_container
+from .helper import lazy_container, numpy_call_wrapper
 
 
-class triton_wrapper(abc.ABC, lazy_container):
+class triton_wrapper(lazy_container, numpy_call_wrapper):
     """
     Wrapper for running triton inference.
 
@@ -154,7 +154,7 @@ class triton_wrapper(abc.ABC, lazy_container):
     Numpy/awkward/dask_awkward inference
     """
 
-    def _validate_numpy_format(
+    def validate_numpy_input(
         self, output_list: List[str], input_dict: Dict[str, numpy.array]
     ) -> None:
         """
@@ -210,7 +210,7 @@ class triton_wrapper(abc.ABC, lazy_container):
                     f"Requested output {oname} not defined by model (Defined: {[x for x in self.model_outputs]})"
                 )
 
-    def _numpy_call(
+    def numpy_call(
         self, output_list: List[str], input_dict: Dict[str, numpy.array], validate=False
     ) -> Dict[str, numpy.array]:
         """
@@ -295,157 +295,3 @@ class triton_wrapper(abc.ABC, lazy_container):
                         (output[o], request.as_numpy(o)), axis=0
                     )
         return {k: v[:orig_len] for k, v in output.items()}
-
-    @abc.abstractclassmethod
-    def awkward_to_numpy(
-        self, *args: awkward.Array, **kwargs: awkward.Array
-    ) -> Dict[str, numpy.array]:
-        """
-        Abstract method to convert arbitrary awkward array inputs into the
-        desired numpy format. Given that there cannot be a fixed input format
-        for inference models, here we provide common interface that users should
-        overload to provide a simple way of abstracting the high-level
-        functions. Since this method explicitly require the conversion to numpy
-        arrays, the input is assumed to be eager/non-dask awkward arrays.
-        """
-        raise NotImplementedError(
-            "This needs to be overloaded by users! "
-            "Consult the following documentation to find the awkward operations needed."
-            "https://awkward-array.org/doc/main/user-guide/how-to-restructure-pad.html"
-        )
-
-    def __call_awkward__(
-        self, output_list: List[str], *args: awkward.Array, **kwargs: awkward.Array
-    ) -> awkward.Array:
-        """
-        Translation function for running inference on awkward arrays. The method
-        for translating the awkward array inputs into numpy arrays will be
-        automatically invoked.
-        """
-        return {
-            k: awkward.from_numpy(v)
-            for k, v in self._numpy_call(
-                output_list, self.awkward_to_numpy(*args, **kwargs)
-            ).items()
-        }
-
-    def dask_touch(self, *args: awkward.Array, **kwargs: awkward.Array) -> None:
-        """
-        Given the input arrays in the format that will be used for
-        awkward_to_numpy format conversion, touching branches of interest. For
-        collection-like arrays (events/jets... etc)This would typically be in be
-        something like `arr.<var>.layout._touch_data(recursive=True)` or
-        `arr.<nested_collection>.<var>.layout._touch_data(recursive=True)`.
-
-        For base implementation, we provide a method that is guaranteed to work
-        but sacrifices performance: recursively touching all branching in the
-        given array.
-        """
-        warnings.warn(
-            "Default implementation is not lazy! "
-            "Users should overload! with branches of interest",
-            DeprecationWarning,
-        )
-        for arr in [*args, *kwargs.values()]:
-            arr.layout._touch_data(recursive=True)
-
-    def __call_dask__(
-        self,
-        output_list: List[str],
-        *args: dask_awkward.Array,
-        **kwargs: dask_awkward.Array,
-    ) -> dask_awkward.Array:
-        """
-        Translation function for running inference on dask awkward arrays.
-        Wrapping the `__call_awkward__` method into a callable object that is
-        suitable to be dask_awkward.map_partition method.
-        """
-
-        class _callable_wrap(convert_args_pair):
-            def __init__(self, inputs, twrap, output_list):
-                """
-                Here we need to also store the args_len and keys argument, as
-                the map_partition method currently only works with *args like
-                arguments. These containers are needed to properly translate the
-                passed *args to a (*args, **kwargs) pair used by
-                __call_awkward__.
-                """
-                super().__init__(inputs)
-                self.twrap = twrap
-                self.output_list = output_list
-
-            def __call__(self, *args):
-                """
-                Mainly translating the input *args to the (*args, **kwarg) pair
-                defined for the `__call_awkward__` method. Additional
-                calculation routine defined to for the 'typetracer' backend for
-                metadata scouting.
-                """
-                if awkward.backend(args[0]) == "typetracer":
-                    # None-recursive touching
-                    # for arr in args:
-                    #     arr.layout._touch_data(recursive=False)
-
-                    # Running the touch overload function
-                    eval_args, eval_kwargs = self.args_to_pair(*args)
-                    self.twrap.dask_touch(*eval_args, **eval_kwargs)
-
-                    # Getting the length-one array for evaluation
-                    eval_args, eval_kwargs = self.args_to_pair(
-                        *tuple(
-                            v.layout.form.length_one_array(behavior=v.behavior)
-                            for v in args
-                        )
-                    )
-
-                    # awkward.zip so that the return is a single awkward
-                    # array
-                    out = awkward.zip(
-                        self.twrap.__call_awkward__(
-                            output_list, *eval_args, **eval_kwargs
-                        )
-                    )
-                    return awkward.Array(
-                        out.layout.to_typetracer(forget_length=True),
-                        behavior=out.behavior,
-                    )
-                else:
-                    eval_args, eval_kwargs = self.args_to_pair(*args)
-                    return awkward.zip(
-                        self.twrap.__call_awkward__(
-                            output_list, *eval_args, **eval_kwargs
-                        )
-                    )
-
-        wrap = _callable_wrap((args, kwargs), self, output_list)
-        arr = dask_awkward.lib.core.map_partitions(
-            wrap,
-            *wrap.pair_to_args(*args, **kwargs),
-            label="triton_wrapper_dak",
-            opt_touch_all=True,
-        )
-        return {o: arr[o] for o in output_list}
-
-    def __call__(
-        self,
-        output_list: List[str],
-        *args: Union[awkward.Array, dask_awkward.Array],
-        **kwargs: Union[awkward.Array, dask_awkward.Array],
-    ) -> Union[awkward.Array, dask_awkward.Array]:
-        """
-        Highest level abstraction to be directly called by the user. The
-        translation of the input arrays (either eager or dask), will be
-        automatically detected. Notice that the input *args, **kwargs should
-        match what is expected for the `awkward_to_numpy` method overloaded by
-        the user.
-        """
-        all_arrs = [*args, *kwargs.values()]
-        if all(isinstance(arr, awkward.Array) for arr in all_arrs):
-            return self.__call_awkward__(output_list, *args, **kwargs)
-        elif all(isinstance(arr, dask_awkward.Array) for arr in all_arrs):
-            return self.__call_dask__(output_list, *args, **kwargs)
-        else:
-            raise ValueError(
-                "All input arrays should be of the same type "
-                "(either awkward.Array or dask_awkward.Array)"
-            )
