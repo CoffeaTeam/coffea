9a26e9d3a938b2fc522618fff3e86f0df2df2863
Yi-Mu Chen <enochnotsocool@gmail.com>
2023-04-27 09:55:42 +0200

    Properly exposing the correct tools

--- coffea/ml_tools/__init__.py
+++ coffea/ml_tools/__init__.py
@@ -4,8 +4,12 @@ Providing the interfaces to the run ML inference such that user can simply
 handle data mangling in awkward/numpy formats. Specifics of passing numpy arrays
 conversion and the handling of dask are mostly abstract away.
 """
+from .helper import numpy_call_wrapper
 from .triton_wrapper import triton_wrapper
+from .torch_wrapper import torch_wrapper
 
 __all__ = [
+    "numpy_call_wrapper",
     "triton_wrapper",
+    "torch_wrapper",
 ]
