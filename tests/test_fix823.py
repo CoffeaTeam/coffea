3bcaacbbd3bf2762a7799498c2c20a3e1da789f0
Lindsey Gray <lindsey.gray@gmail.com>
2023-06-14 06:45:43 -0500

    Update test_fix823.py

--- tests/test_fix823.py
+++ tests/test_fix823.py
@@ -3,7 +3,7 @@ from coffea.nanoevents import NanoAODSchema, NanoEventsFactory
 
 def test_explicit_delete_after_assign():
     events = NanoEventsFactory.from_root(
-        "https://github.com/CoffeaTeam/coffea/raw/master/tests/samples/nano_dy.root",
+        {"https://github.com/CoffeaTeam/coffea/raw/master/tests/samples/nano_dy.root": "Events"},
         metadata={"dataset": "nano_dy"},
         schemaclass=NanoAODSchema,
         permit_dask=True,
