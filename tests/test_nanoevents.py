1b1f4b6dec7c7e9e95ac6dc2b5cead1c1b632f88
Lindsey Gray <lindsey.gray@gmail.com>
2023-10-19 11:23:19 -0500

    test dask separately

--- tests/test_nanoevents.py
+++ tests/test_nanoevents.py
@@ -174,4 +174,8 @@ def test_missing_eventIds_warning():
         factory = NanoEventsFactory.from_root(path, schemaclass=NanoAODSchema)
         events = factory.events()
 
-        events.Muon.pt.compute(scheduler="processes")
+def test_missing_eventIds_warning_dask():
+    path = os.path.abspath("tests/samples/missing_luminosityBlock.root") + ":Events"
+    NanoAODSchema.error_missing_event_ids = False
+    events = NanoEventsFactory.from_root(path, schemaclass=NanoAODSchema, permit_dask=True).events()
+    events.Muon.pt.compute(scheduler="processes")
