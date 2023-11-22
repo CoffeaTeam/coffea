080f2af1c09f287a91bf37e6f1fc4601ca29a1c8
Lindsey Gray <lindsey.gray@gmail.com>
2023-11-22 11:46:10 -0600

    also allow Callables in apply_to_fileset

--- src/coffea/dataset_tools/apply_processor.py
+++ src/coffea/dataset_tools/apply_processor.py
@@ -1,9 +1,15 @@
+import copy
+from typing import Callable, Union
+
 from coffea.nanoevents import NanoAODSchema, NanoEventsFactory
 from coffea.processor import ProcessorABC
 
 
 def apply_to_one_dataset(
-    proc: ProcessorABC, dataset, schemaclass=NanoAODSchema, metadata={}
+    data_manipulation: Union[ProcessorABC, Callable],
+    dataset,
+    schemaclass=NanoAODSchema,
+    metadata={},
 ):
     files = dataset["files"]
     events = NanoEventsFactory.from_root(
@@ -11,13 +17,22 @@ def apply_to_one_dataset(
         metadata=metadata,
         schemaclass=schemaclass,
     ).events()
-    return proc.process(events)
+    if isinstance(data_manipulation, ProcessorABC):
+        return data_manipulation.process(events)
+    elif isinstance(data_manipulation, Callable):
+        return data_manipulation(events)
+    else:
+        raise ValueError("data_manipulation must either be a ProcessorABC or Callable")
 
 
-def apply_to_fileset(proc: ProcessorABC, fileset, schemaclass=NanoAODSchema):
+def apply_to_fileset(
+    data_manipulation: Union[ProcessorABC, Callable], fileset, schemaclass=NanoAODSchema
+):
     out = {}
     for name, dataset in fileset.items():
-        metadata = dataset.get("metadata", {}).copy()
+        metadata = copy.deepcopy(dataset.get("metadata", {}))
         metadata["dataset"] = name
-        out[name] = apply_to_one_dataset(proc, dataset, schemaclass, metadata)
+        out[name] = apply_to_one_dataset(
+            data_manipulation, dataset, schemaclass, metadata
+        )
     return out
