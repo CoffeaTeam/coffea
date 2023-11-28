7644fe71de0793e744a54d2c2ab6c145fc4e3336
Lindsey Gray <lindsey.gray@gmail.com>
2023-11-28 17:14:35 -0600

    more typing for apply_processor

--- src/coffea/dataset_tools/apply_processor.py
+++ src/coffea/dataset_tools/apply_processor.py
@@ -1,33 +1,40 @@
+from __future__ import annotations
+
 import copy
-from typing import Callable, Dict, Hashable, List, Set, Tuple, Union
+from typing import Any, Callable, Dict, Hashable, List, Set, Tuple, Union
 
 import dask.base
 import dask_awkward
 
-from coffea.nanoevents import NanoAODSchema, NanoEventsFactory
+from coffea.dataset_tools.preprocess import (
+    DatasetSpec,
+    DatasetSpecOptional,
+    FilesetSpec,
+    FilesetSpecOptional,
+)
+from coffea.nanoevents import BaseSchema, NanoAODSchema, NanoEventsFactory
 from coffea.processor import ProcessorABC
 
-GenericHEPAnalysis = Callable[
-    [dask_awkward.Array],
-    Tuple[
-        Union[
-            dask.base.DaskMethodsMixin,
-            Dict[Hashable, dask.base.DaskMethodsMixin],
-            Set[dask.base.DaskMethodsMixin],
-            List[dask.base.DaskMethodsMixin],
-            Tuple[dask.base.DaskMethodsMixin],
-        ],
-        ...,
-    ],  # NOTE TO USERS: You can use nested python containers as arguments to dask.compute!
-]
+DaskOutputType = Tuple[
+    Union[
+        dask.base.DaskMethodsMixin,
+        Dict[Hashable, dask.base.DaskMethodsMixin],
+        Set[dask.base.DaskMethodsMixin],
+        List[dask.base.DaskMethodsMixin],
+        Tuple[dask.base.DaskMethodsMixin],
+    ],
+    ...,
+]  # NOTE TO USERS: You can use nested python containers as arguments to dask.compute!
+
+GenericHEPAnalysis = Callable[[dask_awkward.Array], DaskOutputType]
 
 
 def apply_to_dataset(
-    data_manipulation: Union[ProcessorABC, GenericHEPAnalysis],
-    dataset,
-    schemaclass=NanoAODSchema,
-    metadata={},
-):
+    data_manipulation: ProcessorABC | GenericHEPAnalysis,
+    dataset: DatasetSpec | DatasetSpecOptional,
+    schemaclass: BaseSchema = NanoAODSchema,
+    metadata: dict[Hashable, Any] = {},
+) -> DaskOutputType:
     files = dataset["files"]
     events = NanoEventsFactory.from_root(
         files,
@@ -43,10 +50,10 @@ def apply_to_dataset(
 
 
 def apply_to_fileset(
-    data_manipulation: Union[ProcessorABC, GenericHEPAnalysis],
-    fileset,
-    schemaclass=NanoAODSchema,
-):
+    data_manipulation: ProcessorABC | GenericHEPAnalysis,
+    fileset: FilesetSpec | FilesetSpecOptional,
+    schemaclass: BaseSchema = NanoAODSchema,
+) -> dict[str, DaskOutputType]:
     out = {}
     for name, dataset in fileset.items():
         metadata = copy.deepcopy(dataset.get("metadata", {}))
