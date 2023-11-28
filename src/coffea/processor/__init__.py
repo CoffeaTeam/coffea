5e127c25911eea67178e48ec0b8b112fe259f530
Lindsey Gray <lindsey.gray@gmail.com>
2023-11-28 11:08:29 -0600

    remove accumulator concept

--- src/coffea/processor/__init__.py
+++ src/coffea/processor/__init__.py
@@ -2,26 +2,8 @@
 
 
 """
-from .accumulator import (
-    AccumulatorABC,
-    accumulate,
-    column_accumulator,
-    defaultdict_accumulator,
-    dict_accumulator,
-    list_accumulator,
-    set_accumulator,
-    value_accumulator,
-)
 from .processor import ProcessorABC
 
 __all__ = [
-    "column_accumulator",
-    "defaultdict_accumulator",
-    "dict_accumulator",
-    "list_accumulator",
-    "set_accumulator",
-    "value_accumulator",
-    "accumulate",
-    "AccumulatorABC",
     "ProcessorABC",
 ]
