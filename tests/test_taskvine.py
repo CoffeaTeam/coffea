a1b8bcb84061b1a2462bee6c41b24d739839cf3a
Benjamin Tovar <btovar@nd.edu>
2023-12-04 12:23:20 -0500

    test: small coffea+dask+taskvine example (from benchmarks)

---
+++ tests/test_taskvine.py
@@ -0,0 +1,90 @@
+from coffea.nanoevents import NanoEventsFactory
+from coffea import processor
+import hist.dask as hda
+
+from sys import version_info
+import os
+
+
+def histogram_common():
+    # The opendata files are non-standard NanoAOD, so some optional data columns are missing
+    processor.NanoAODSchema.warn_missing_crossrefs = False
+
+    # "file:/tmp/Run2012B_SingleMu.root",
+    events = NanoEventsFactory.from_root(
+        {os.path.abspath("tests/samples/nano_dy.root"): "Events"},
+        chunks_per_file=4,
+        metadata={"dataset": "SingleMu"},
+    ).events()
+
+    q1_hist = (
+        hda.Hist.new.Reg(100, 0, 200, name="met", label="$E_{T}^{miss}$ [GeV]")
+        .Double()
+        .fill(events.MET.pt)
+    )
+
+    return q1_hist
+
+
+def test_taskvine_local_env():
+    try:
+        from ndcctools.taskvine import DaskVine, Factory
+    except ImportError:
+        print("taskvine is not installed. Omitting test.")
+        return
+
+    m = DaskVine(port=0)
+    workers = Factory(manager=m, batch_type="local")
+    workers.min_workers = 1
+    workers.max_workers = 1
+    workers.cores = 1
+
+    q1_hist = histogram_common()
+    with workers:
+        result = q1_hist.compute(
+            scheduler=m.get, resources={"cores": 1}, resources_mode=None
+        )
+        assert result.sum() == 40.0
+
+
+def test_taskvine_remote_env():
+    try:
+        from ndcctools.taskvine import DaskVine, Factory
+        from ndcctools.poncho import package_create
+    except ImportError:
+        print("taskvine is not installed. Omitting test.")
+        return
+
+    py_version = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
+
+    poncho_spec = {
+        "conda": {
+            "channels": ["conda-forge"],
+            "dependencies": [
+                f"python={py_version}",
+                "ndcctools",
+                {"pip": ["coffea=.[dask]"]},
+            ],
+        },
+    }
+
+    env_filename = "vine-env.tar.gz"
+    package_create.pack_env_from_dict(poncho_spec, env_filename)
+
+    m = DaskVine(port=0)
+    env = m.declare_poncho(env_filename, cache=True)
+
+    workers = Factory(manager=m, batch_type="local")
+    workers.min_workers = 1
+    workers.max_workers = 1
+    workers.cores = 1
+
+    q1_hist = histogram_common()
+    with workers:
+        result = q1_hist.compute(
+            scheduler=m.get,
+            resources={"cores": 1},
+            resources_mode=None,
+            environment=env,
+        )
+        assert result.sum() == 40.0
