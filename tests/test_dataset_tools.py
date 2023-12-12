6d0722cbdb7e0ccea3ac8a4f28fb83df2a237996
Lindsey Gray <lindsey.gray@gmail.com>
2023-12-12 11:37:22 -0400

    adjust uproot pins, add test

--- tests/test_dataset_tools.py
+++ tests/test_dataset_tools.py
@@ -2,7 +2,13 @@ import dask
 import pytest
 from distributed import Client
 
-from coffea.dataset_tools import apply_to_fileset, max_chunks, preprocess, slice_chunks
+from coffea.dataset_tools import (
+    apply_to_fileset,
+    get_failed_steps_for_fileset,
+    max_chunks,
+    preprocess,
+    slice_chunks,
+)
 from coffea.nanoevents import BaseSchema, NanoAODSchema
 from coffea.processor.test_items import NanoEventsProcessor, NanoTestProcessor
 
@@ -28,6 +34,56 @@ _starting_fileset = {
         "files": {
             "tests/samples/nano_dimuon.root": "Events",
             "tests/samples/nano_dimuon_not_there.root": "Events",
+        }
+    },
+}
+
+_starting_fileset_with_steps = {
+    "ZJets": {
+        "files": {
+            "tests/samples/nano_dy.root": {
+                "object_path": "Events",
+                "steps": [
+                    [0, 5],
+                    [5, 10],
+                    [10, 15],
+                    [15, 20],
+                    [20, 25],
+                    [25, 30],
+                    [30, 35],
+                    [35, 40],
+                ],
+            }
+        }
+    },
+    "Data": {
+        "files": {
+            "tests/samples/nano_dimuon.root": {
+                "object_path": "Events",
+                "steps": [
+                    [0, 5],
+                    [5, 10],
+                    [10, 15],
+                    [15, 20],
+                    [20, 25],
+                    [25, 30],
+                    [30, 35],
+                    [35, 40],
+                ],
+            },
+            "tests/samples/nano_dimuon_not_there.root": {
+                "object_path": "Events",
+                "steps": [
+                    [0, 5],
+                    [5, 10],
+                    [10, 15],
+                    [15, 20],
+                    [20, 25],
+                    [25, 30],
+                    [30, 35],
+                    [35, 40],
+                ],
+            },
         }
     },
 }
@@ -217,4 +273,36 @@ def test_slicechunks():
                 }
             }
         },
+    }
+
+
+def test_recover_failed_chunks():
+    with Client() as _:
+        to_compute = apply_to_fileset(
+            NanoEventsProcessor(),
+            _starting_fileset_with_steps,
+            schemaclass=NanoAODSchema,
+            uproot_options={"allow_read_errors_with_report": True},
+        )
+        out, reports = dask.compute(*to_compute)
+
+    failed_fset = get_failed_steps_for_fileset(_starting_fileset_with_steps, reports)
+    assert failed_fset == {
+        "Data": {
+            "files": {
+                "tests/samples/nano_dimuon_not_there.root": {
+                    "object_path": "Events",
+                    "steps": [
+                        [0, 5],
+                        [5, 10],
+                        [10, 15],
+                        [15, 20],
+                        [20, 25],
+                        [25, 30],
+                        [30, 35],
+                        [35, 40],
+                    ],
+                }
+            }
+        }
     }
