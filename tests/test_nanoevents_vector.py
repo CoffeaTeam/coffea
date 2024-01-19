1025b887bb88ac741261f46d7ada03e978c85f90
Lindsey Gray <lindsey.gray@gmail.com>
2024-01-19 14:46:58 -0600

    fix: test delta-r + metric return with and without optimization

--- tests/test_nanoevents_vector.py
+++ tests/test_nanoevents_vector.py
@@ -537,13 +537,14 @@ def test_inherited_method_transpose(lcoord, threecoord, twocoord):
     assert record_arrays_equal(b - c, -(c - b))
 
 
-def test_dask_metric_table_and_nearest():
+@pytest.mark.parametrize("optimization_enabled", [True, False])
+def test_dask_metric_table_and_nearest(optimization_enabled):
     import dask
     from dask_awkward.lib.testutils import assert_eq
 
     from coffea.nanoevents import NanoEventsFactory
 
-    with dask.config.set({"awkward.optimization.enabled": False}):
+    with dask.config.set({"awkward.optimization.enabled": optimization_enabled}):
         eagerevents = NanoEventsFactory.from_root(
             {"tests/samples/nano_dy.root": "Events"},
             delayed=False,
