c42a68ba373f5bdf0d13a3cfd1da5a01d14ffcc9
Lindsey Gray <lindsey.gray@gmail.com>
2024-01-25 13:45:24 -0600

    explain more about internal migrations

--- src/coffea/nanoevents/methods/vector.py
+++ src/coffea/nanoevents/methods/vector.py
@@ -56,7 +56,7 @@ from coffea.util import deprecate
 _cst = pytz.timezone("US/Central")
 _depttime = _cst.localize(datetime(2024, 6, 30, 11, 59, 59))
 deprecate(
-    "coffea.nanoevents.methods.vector will be removed and replaced with scikit-hep vector. Consider using that package!",
+    "coffea.nanoevents.methods.vector will be removed and replaced with scikit-hep vector. Nanoevents schemas internal to coffea will be migrated. Otherwise please consider using that package!",
     version="2024.7.0",
     date=str(_depttime),
     category=FutureWarning,
